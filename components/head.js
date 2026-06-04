e6a4d4bba8309f601e4719fc000f688495c30d81
squad-life <bahrtest6424@gmail.com>
2026-06-04 14:14:03 +0300

    Create head.js

---
+++ components/head.js
@@ -0,0 +1,8 @@
+document.head.insertAdjacentHTML('afterbegin', `
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no">
+    <title>Life Shop — донат в игры</title>
+    <script src="https://telegram.org/js/telegram-web-app.js"><\/script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="/css/main.css">
+`);
